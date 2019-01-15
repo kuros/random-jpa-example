c4185eabad25089f5f51383837de4e61cdbd0896
kumarro <rohit.kumar@cdk.com>
2019-01-15 23:48:15 +0530

    Query fix

--- src/main/java/in/kuros/randomjpa/blogexample/repository/SalaryRepository.java
+++ src/main/java/in/kuros/randomjpa/blogexample/repository/SalaryRepository.java
@@ -12,6 +12,6 @@ import java.util.List;
 public interface SalaryRepository extends PagingAndSortingRepository<Salary, Integer> {
 
     @RestResource(path = "latest")
-    @Query("FROM Salary s WHERE NOT EXISTS (select 1 FROM Salary s2 WHERE s.employeeId = s2.employeeId and s2.fromDate < s.fromDate and s2.toDate < s.toDate) ")
+    @Query("FROM Salary s WHERE NOT EXISTS (select 1 FROM Salary s2 WHERE s.employeeId = s2.employeeId and s.fromDate < s2.fromDate and s.toDate < s2.toDate) ")
     List<Salary> findLatestSalaries();
 }
