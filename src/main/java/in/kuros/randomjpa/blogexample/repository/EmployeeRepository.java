4b87f1bd82ceac16a0cfb43b7df2781c9c2825ad
kumarro <rohit.kumar@cdk.com>
2019-01-15 23:22:05 +0530

    renamed entities

--- src/main/java/in/kuros/randomjpa/blogexample/repository/EmployeeRepository.java
+++ src/main/java/in/kuros/randomjpa/blogexample/repository/EmployeeRepository.java
@@ -1,9 +1,9 @@
 package in.kuros.randomjpa.blogexample.repository;
 
-import in.kuros.randomjpa.blogexample.entity.Employees;
+import in.kuros.randomjpa.blogexample.entity.Employee;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource(collectionResourceRel = "employee", path = "employee")
-public interface EmployeeRepository extends PagingAndSortingRepository<Employees, Integer> {
+public interface EmployeeRepository extends PagingAndSortingRepository<Employee, Integer> {
 }
